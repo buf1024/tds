927ee4769f56a211e5774fde0ccbccc5607b7791
buf1024 <buf1024@gmail.com>
2014-01-19 00:01:42 +0800

    setup env

---
+++ test/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 2013-12-28
+ *      Author: Luo Guochun
+ */
+#define C_TEST_APP
+
+#include "ctest.h"
+
+int main(int argc, char **argv)
+{
+    INIT_TEST(argc, argv);
+    return RUN_ALL_TEST();
+}
